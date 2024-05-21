fe32f72f0b6cd8beb00d57d7dffa2662e79953c4
ilesovvv <zarbdorilyosov@gmail.com>
2024-05-21 20:33:37 +0300

    Создал html страницы Курсов, и о нас

--- education_main/app/urls.py
+++ education_main/app/urls.py
@@ -1,8 +1,11 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about_us', views.about_us, name='about_us')
+    path('about/', views.about, name='about'),
+    path('course1/', views.course1, name='course1'),
+    path('course2/', views.course2, name='course2'),
+    path('course3/', views.course3, name='course3')
 ]
--- education_main/app/views.py
+++ education_main/app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
-def about_us(request):
+def course1(request):
+    return render(request, 'course-grid-1.html', {})
+
+def course2(request):
+    return render(request, 'course-grid-2.html', {})
+
+def course3(request):
+    return render(request, 'course-grid-3.html', {})
+
+def about(request):
     return render(request, 'about.html', {})
