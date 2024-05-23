433e1c0606606fdafc6a6a4ccb9893041ab02b97
FiFanta <jarkovkirill15@yandex.ru>
2024-05-23 21:07:23 +0300

    созданы модели учителя студента и курсов

--- education_main/app/models.py
+++ education_main/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Teacher(models.Model): # tr = teacher
+    firstname_tr = models.CharField(max_length=255) # tr = teacher
+    surname_tr = models.CharField(max_length=255) # tr = teacher
+    patronymic_tr = models.CharField(max_length=255) # tr = teacher
+    mail_tr = models.EmailField()  # tr = teacher
+    birthday_tr = models.DateField() # tr = teacher
+    date_created_account_tr = models.DateField(auto_now_add=True) # tr = teacher
+    conducting_courses_tr = models.ManyToManyField() # tr = teacher
+    slug = models.SlugField(unique=True)
+    """ss
+    slug учителя. Нужен или нет? 
+    """
+
+class Students(models.Model): # st = student
+    firstname_st = models.CharField(max_length=255) # st = student
+    surname_st = models.CharField(max_length=255) # st = student
+    patronymic_st = models.CharField(max_length=255) # st = student
+    mail_st = models.EmailField()  # st = student
+    date_created_account_st = models.DateField(auto_now_add=True) # st = student
+    part_courses_st = models.ManyToManyField() # st = student
+    
+class Courses(models.Model):
+    name_course = models.CharField(max_length=255)
+    price_course = models.CharField(max_length=255)
+    likes = models.ManyToManyField(User, related_name='likes')
+    views = models.ManyToManyField(User, related_name='views_quantity')
+    
