b617fccec81c87e5a078dbd8b0bee27b4b3e6ddd
sss <sss>
2024-06-13 20:49:03 +0300

    Изменение приложения teacher

--- education_main/teacher/views.py
+++ education_main/teacher/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def teacher_cabinet(request):
+    students = User.objects.filter(groups__name="Студенты")
     context = {
         'title': 'Список студентов',
-        'school': 'school'
+        'school': 'school',
+        'students': students
     }
     return render(request, 'teacher_cabinet.html', context)
