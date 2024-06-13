10379dd88a6c1295d1b4869f6a37845468acaee1
sss <sss>
2024-06-13 22:13:33 +0300

    Попытка добавления функционала и таблиц

--- education_main/manager/urls.py
+++ education_main/manager/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('requests', views.requests, name='requests'),
     path('manager_cabinet', views.manager_cabinet, name='manager_cabinet'),
+    path('delete_student/<int:pk>', views.delete_student, name='delete_student'),
     
 ]
